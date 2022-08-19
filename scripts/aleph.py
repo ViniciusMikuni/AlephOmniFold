6ec2e43afd1f9b433665584548e44daa3bc2a4db
ViniciusMikuni <vinicius.mikuni@gmail.com>
2022-08-18 22:45:52 -0700

    fix file path

--- scripts/aleph.py
+++ scripts/aleph.py
@@ -23,7 +23,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--config', default='config_omnifold.json', help='Basic config file containing general options')
 parser.add_argument('--plot_folder', default='../plots', help='Folder used to store plots')
-parser.add_argument('--file_path', default='/pscratch/sd/v/vmikuni/aleph', help='Folder containing input files')
+parser.add_argument('--file_path', default='/global/cfs/cdirs/m3246/bnachman/LEP/aleph/processed/', help='Folder containing input files')
 parser.add_argument('--nevts', type=float,default=-1, help='Dataset size to use during training')
 parser.add_argument('--verbose', action='store_true', default=False,help='Run the scripts with more verbose output')
 
