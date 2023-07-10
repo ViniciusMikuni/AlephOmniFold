7712b0d15034f970a8c97969a453c51c3a861d59
mavaylon1 <mavaylon1@berkeley.edu>
2023-07-10 14:57:46 -0700

    clean

--- scripts/.ipynb_checkpoints/bootstrap_plot-Copy1-checkpoint.py
+++ scripts/.ipynb_checkpoints/bootstrap_plot-Copy1-checkpoint.py
@@ -29,10 +29,11 @@ opt = LoadJson(flags.config)
 itrials=0
 
 data, mc_reco,mc_gen,reco_mask,gen_mask = utils.DataLoader(flags.file_path,opt,nevts)
-# breakpoint()
-# estimate statistical uncertainty 
+#############################################
+"""Estimate Statistical Uncertainty"""
+#############################################
+
 sim_det_data = 1-mc_reco[reco_mask]
-# breakpoint()
 output_sim_data, bins = np.histogram(sim_det_data, bins=utils.binning, density=False)
 est_sim_stat = 1/np.sqrt(output_sim_data)
 
@@ -40,6 +41,9 @@ real_data = 1-data[:,0]
 output_real_data, bins = np.histogram(real_data, bins=utils.binning, density=False)
 est_real_stat = 1/np.sqrt(output_real_data)
 
+#############################################
+"""Ensemble of 10 groups of 40"""
+#############################################
 weights_paths= glob('/pscratch/sd/m/mavaylon/redone_enemble_weights/*')
 
 rem = len(weights_paths)%40
@@ -67,17 +71,12 @@ for group in tqdm(set_40):
 
         feed_dict_data=1-mc_gen[gen_mask][:,0]
         output_data, bins = np.histogram(feed_dict_data, bins=utils.binning, weights = omnifold_weights[gen_mask], density=False)
-        # print(output_data)
         hist_sessions_data.append(output_data)
         bins_.append(bins)
 
-    # sd = np.std(hist_sessions_data,axis=0)
     ave = np.mean(hist_sessions_data,axis=0)
     session_dict['group_'+str(group_num)] = ave
-    # sd_over_mean = sd/ave
     group_num=group_num+1
-
-# pprint(session_dict)
 
 # calculate sd of the averages
 sd_40 = np.std(list(session_dict.values()),axis=0)
@@ -86,10 +85,11 @@ sd_over_mean_40 = sd_40/ave_40
 
 # keys to present sd of ave per bin
 bins = ['bin_'+str(i) for i in utils.binning]
-sd_dict_of_ave_bins_40 = dict(zip(bins, sd_40))
-# pprint(sd_dict_of_ave_bins)
 
-#subset of 40 
+#############################################
+"""subset of 40 """
+#############################################
+
 subset_40 = sets_of_40[1]
 
 sub_hist_sessions_data =[]
@@ -109,26 +109,9 @@ sub_sd_40 = np.std(sub_hist_sessions_data,axis=0)
 sub_ave_40 = np.mean(sub_hist_sessions_data,axis=0)
 sub_sd_over_mean_40 = sub_sd_40/sub_ave_40
 
-sd_dict_sub_40 = dict(zip(bins,sub_sd_40))
-
-
-
-# #Bootstrap Full n=10
-# parser = argparse.ArgumentParser()
-
-# parser.add_argument('--config', default='config_omnifold.json', help='Basic config file containing general options')
-# parser.add_argument('--plot_folder', default='../plots', help='Folder used to store plots')
-# parser.add_argument('--file_path', default='/global/cfs/cdirs/m3246/bnachman/LEP/aleph/processed', help='Folder containing input files')
-# parser.add_argument('--nevts', type=float, default=-1, help='Dataset size to use during training')
-# parser.add_argument('--verbose', action='store_true', default=False,help='Run the scripts with more verbose output')
-
-
-# flags = parser.parse_args()
-# nevts=int(flags.nevts)
-# opt = LoadJson(flags.config)
-# itrials=0
-
-# data, mc_reco,mc_gen,reco_mask,gen_mask = utils.DataLoader(flags.file_path,opt,nevts)
+#############################################
+"""Bootstrap Full StrapN 10"""
+#############################################
 # boot_folders=['/pscratch/sd/m/mavaylon/phys_bootstrap/OmniFold/boot_new_double_check/new_boot_'+str(i)+'/*' for i in range(1,11)]
 
     
@@ -159,26 +142,11 @@ sd_dict_sub_40 = dict(zip(bins,sub_sd_40))
 # ave_full = np.mean(list(session_dict.values()),axis=0)
 # sd_over_mean_full = sd_full/ave_full
 
-#Bootstrap Full n=40
-parser = argparse.ArgumentParser()
-
-parser.add_argument('--config', default='config_omnifold.json', help='Basic config file containing general options')
-parser.add_argument('--plot_folder', default='../plots', help='Folder used to store plots')
-parser.add_argument('--file_path', default='/global/cfs/cdirs/m3246/bnachman/LEP/aleph/processed', help='Folder containing input files')
-parser.add_argument('--nevts', type=float, default=-1, help='Dataset size to use during training')
-parser.add_argument('--verbose', action='store_true', default=False,help='Run the scripts with more verbose output')
-
-
-flags = parser.parse_args()
-nevts=int(flags.nevts)
-opt = LoadJson(flags.config)
-itrials=0
-
-data, mc_reco,mc_gen,reco_mask,gen_mask = utils.DataLoader(flags.file_path,opt,nevts)
+#############################################
+"""Bootstrap Full StrapN=40"""
+#############################################
 boot_folders=['/pscratch/sd/m/mavaylon/phys_weights_n_40/bootstrap_n_'+str(i)+'/*' for i in range(1,41)]
 
-
-    
 boot_n = 1
 session_dict={}
 for boot in boot_folders:
@@ -206,26 +174,11 @@ sd_full_40 = np.std(list(session_dict.values()),axis=0)
 ave_full_40 = np.mean(list(session_dict.values()),axis=0)
 sd_over_mean_full_40 = sd_full_40/ave_full_40
 
-#Bootstrap Sim Full n=40
-parser = argparse.ArgumentParser()
-
-parser.add_argument('--config', default='config_omnifold.json', help='Basic config file containing general options')
-parser.add_argument('--plot_folder', default='../plots', help='Folder used to store plots')
-parser.add_argument('--file_path', default='/global/cfs/cdirs/m3246/bnachman/LEP/aleph/processed', help='Folder containing input files')
-parser.add_argument('--nevts', type=float, default=-1, help='Dataset size to use during training')
-parser.add_argument('--verbose', action='store_true', default=False,help='Run the scripts with more verbose output')
-
-
-flags = parser.parse_args()
-nevts=int(flags.nevts)
-opt = LoadJson(flags.config)
-itrials=0
-
-data, mc_reco,mc_gen,reco_mask,gen_mask = utils.DataLoader(flags.file_path,opt,nevts)
+#############################################
+"""Bootstrap Sim Full n=40"""
+#############################################
 boot_folders=['/pscratch/sd/m/mavaylon/boot_sim_n_40/boot_sim_'+str(i)+'/*' for i in range(1,41)]
 
-
-    
 boot_n = 1
 session_dict={}
 for boot in boot_folders:
@@ -253,23 +206,9 @@ sd_sim_full_40 = np.std(list(session_dict.values()),axis=0)
 ave_sim_full_40 = np.mean(list(session_dict.values()),axis=0)
 sd_over_mean_full_40_sim = sd_sim_full_40/ave_sim_full_40
 
-
-# #Bootstrap Quarter
-# parser = argparse.ArgumentParser()
-
-# parser.add_argument('--config', default='config_omnifold.json', help='Basic config file containing general options')
-# parser.add_argument('--plot_folder', default='../plots', help='Folder used to store plots')
-# parser.add_argument('--file_path', default='/global/cfs/cdirs/m3246/bnachman/LEP/aleph/processed', help='Folder containing input files')
-# parser.add_argument('--nevts', type=float, default=-1, help='Dataset size to use during training')
-# parser.add_argument('--verbose', action='store_true', default=False,help='Run the scripts with more verbose output')
-
-
-# flags = parser.parse_args()
-# nevts=int(flags.nevts)
-# opt = LoadJson(flags.config)
-# itrials=0
-
-# data, mc_reco,mc_gen,reco_mask,gen_mask = utils.DataLoader(flags.file_path,opt,nevts)
+#############################################
+"""Bootstrap Quarter"""
+#############################################
 # boot_folders=['/pscratch/sd/m/mavaylon/phys_bootstrap/OmniFold/boot_quarter_stratified_check/boot_quarter_'+str(i)+'/*' for i in range(1,11)]
 
 
